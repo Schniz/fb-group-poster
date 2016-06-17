4b2bcc5f000cd5dcc55975ff307669d130806d90
Gal Schlezinger <galstar@spitfire.co.il>
2016-06-17 20:02:57 +0300

    works

--- authenticate.js
+++ authenticate.js
@@ -2,13 +2,33 @@ var webdriver = require('selenium-webdriver');
 var By = webdriver.By;
 var until = webdriver.until;
 
-module.exports = function authenticate(user, pass, cb) {
+module.exports = function authenticate(user, pass, data, cb) {
+	var msg = data.msg;
+	var groupId = data.groupId;
 	var driver = new webdriver.Builder().forBrowser('firefox').build();
+
 	driver.get('https://facebook.com/login.php');
 	driver.findElement(By.id('email')).sendKeys(user);
 	driver.findElement(By.id('pass')).sendKeys(pass);
 	driver.findElement(By.id('pass')).sendKeys(webdriver.Key.RETURN);
-	driver.wait(until.titleIs('Facebook')).then(() => {
+	driver.wait(until.titleIs('Facebook'));
+	driver.get('https://www.facebook.com/groups/' + groupId);
+
+	var textareaCss = '[data-testid=react-composer-root] textarea';
+	driver.wait(until.elementLocated(By.css(textareaCss)));
+	driver.findElement(By.css(textareaCss)).click();
+
+	var composerCss = '[data-testid=react-composer-root] [contenteditable]';
+	var composer = () => driver.findElement(By.css(composerCss));
+	driver.wait(until.elementLocated(By.css(composerCss)));
+	composer().sendKeys(msg);
+	composer().sendKeys(webdriver.Key.RETURN);
+
+	driver.sleep(3000);
+
+	driver.executeScript(() => {
+		document.querySelector('[data-testid=react-composer-post-button]').click();
+	}).then(() => {
 		cb(null, {
 			kill: () => driver.quit()
 		});
--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var authenticate = require('./authenticate');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 
 var port = process.env.PORT || 8000;
 var app = express();
 var USER_EMAIL = process.env.USER_EMAIL;
 var USER_PASS = process.env.USER_PASS;
+var SECRET = process.env.SECRET;
 
-app.get('/', (req, res) => {
-	authenticate(USER_EMAIL, USER_PASS, (err, result) => {
-		res.send('authenticated!');
-		result.kill();
-	});
+app.post('/', bodyParser.json(), (req, res) => {
+	console.log('got body', req.body);
+	var signedMessage = req.body.signed;
+	try {
+		var message = jwt.verify(signedMessage, SECRET);
+		var msg = message.text;
+		var groupId = message.groupId;
+
+		authenticate(USER_EMAIL, USER_PASS, { msg, groupId }, (err, result) => {
+			res.send({ status: 'ok' });
+			setTimeout(() => {
+				result.kill();
+			}, 10000);
+		});
+	} catch(e) {
+		console.log('an error occured', e);
+		res.send({ status: 'error' });
+	}
 });
 
 app.listen(port, () => {
